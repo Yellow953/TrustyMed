8a7434bc6231e577221b729f3d961c9f5427e56a
joe953 <joemazloum953@gmail.com>
2023-03-12 17:34:59 +0200

    login register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -70,4 +70,45 @@
         </div>
     </div>
 </div>
+@endsection --}}
+
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-fluid py-5 px-2">
+        <div class="row justify-content-center mx-4">
+            <div class="col-10 text-center my-4">
+                <img src="{{asset('assets/images/image1.png')}}" alt="">
+                <h1 class="mt-3">Welcome Back</h1>
+            </div>
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <div class="col-12 col-md-6 mx-auto my-4">
+                    <input type="email" class="form-control custom-field" name="email" placeholder="Email" required>
+                </div>
+                <div class="col-12 col-md-6 mx-auto my-4">
+                    <input type="password" class="form-control custom-field" name="password" placeholder="********" required>
+                </div>
+                <div class="col-md-4 offset-md-4 my-4">
+                    <button type="submit" class="custom-btn">Login</button>
+                </div>
+            </form>
+            <div class="d-flex">
+                <span class="gr-1 my-auto"></span>
+                <span class="w-50 text-center">Or Sign Up With</span>
+                <span class="gr-2 my-auto"></span>
+            </div>
+            <div class="d-flex justify-content-center my-3">
+                <div class="rounded-full bg-custom-gray mx-2">
+                    <img src="{{asset('assets/images/google.png')}}" alt="">
+                </div>
+                <div class="rounded-full bg-custom-gray mx-2">
+                    <img src="{{asset('assets/images/facebook.png')}}" alt="">
+                </div>
+                <div class="rounded-full bg-custom-gray mx-2">
+                    <img src="{{asset('assets/images/apple.png')}}" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+    <div class="container-fluid py-5 px-2">
+        <div class="row justify-content-center mx-4">
+            <div class="col-6 text-center my-4">
+                <img src="{{asset('assets/images/image1.png')}}" alt="">
+                <h1 class="mt-3">Welcome</h1>
             </div>
+            <form method="POST" action="{{ route('register') }}">
+                @csrf
+                <div class="col-12 col-lg-6 mx-auto my-4">
+                    <div class="row">
+                        <div class="col-6">
+                            <input type="text" class="form-control custom-field" name="first_name" placeholder="First Name" required>
+                        </div>
+                        <div class="col-6">
+                            <input type="text" class="form-control custom-field" name="last_name" placeholder="Last Name" required>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-6 mx-auto">
+                    <input type="date" class="form-control custom-field" name="dob" placeholder="Date of Birth" required>
+                </div>
+                <div class="col-12 col-lg-6 mx-auto my-4">
+                    <select name="gender" class="form-control custom-field" required>
+                        <option>Gender</option>
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
+                        <option value="other">Other</option>
+                    </select>
+                </div>
+                <div class="col-12 col-lg-6 mx-auto my-4">
+                    <input type="text" class="form-control custom-field" name="address" placeholder="Address" required>
+                </div>
+                <div class="col-12 col-lg-6 mx-auto my-4">
+                    <input type="text" class="form-control custom-field" name="phone" placeholder="Phone Number" required>
+                </div>
+                <div class="col-12 col-lg-6 mx-auto my-4">
+                    <input type="email" class="form-control custom-field" name="email" placeholder="Email" required>
+                </div>
+                <div class="col-12 col-lg-6 mx-auto my-4">
+                    <input type="password" class="form-control custom-field" name="password" placeholder="********" required>
+                </div>
+                <div class="col-12 col-lg-6 mx-auto my-4">
+                    <input type="password" class="form-control custom-field" name="password_confirmation" placeholder="********" required>
+                </div>
+                <div class="col-md-4 offset-md-4 my-4">
+                    <button type="submit" class="custom-btn">Create Account</button>
+                </div>
+            </form>    
         </div>
     </div>
-</div>
 @endsection
